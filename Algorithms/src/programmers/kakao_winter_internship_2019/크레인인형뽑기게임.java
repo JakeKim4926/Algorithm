aec76f126eb6565dfff92681c9e482134ea72585
Jake_Kim <wer1915@gmail.com>
2024-07-02 10:06:52 +0900

    [feat] add 크레인인형뽑기게임 in programmers

---
+++ Algorithms/src/programmers/kakao_winter_internship_2019/크레인인형뽑기게임.java
@@ -0,0 +1,40 @@
+package programmers.kakao_winter_internship_2019;
+
+import java.util.*;
+
+public class 크레인인형뽑기게임 {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        boolean[][] bBoard = new boolean[board.length][board[0].length];
+
+        Stack<Integer> pocket = new Stack<>();
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == 0)
+                    bBoard[i][j] = true;
+            }
+        }
+
+        for(int i = 0 ; i< moves.length; i++) {
+            int location = moves[i]-1;
+            for(int j = 0; j < board.length; j++) {
+                if(!bBoard[j][location]) {
+                    if(pocket.isEmpty() || pocket.peek() != board[j][location]) {
+                        pocket.push(board[j][location]);
+                    }
+                    else {
+                        pocket.pop();
+                        answer+=2;
+                    }
+
+                    bBoard[j][location] = true;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
