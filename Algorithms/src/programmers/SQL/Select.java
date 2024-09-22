b0ed4455432b1244649d2f965f651894b729feaf
김준섭 <wns1915@naver.com>
2024-09-22 23:05:53 +0900

    [feat] add 3월에 태어난 여성 회원 목록 출력하기in Programmers [SQL]

--- Algorithms/src/programmers/SQL/Select.java
+++ Algorithms/src/programmers/SQL/Select.java
@@ -28,6 +28,14 @@ public class Select {
 //    WHERE A.TOTAL_ORDER > 3000
 //    AND B.INGREDIENT_TYPE='fruit_based'
 //    ORDER BY A.TOTAL_ORDER DESC;
+    
+//    3월에 태어난 여성 회원 목록 출력하기
+//    SELECT MEMBER_ID,MEMBER_NAME,GENDER,DATE_FORMAT(DATE_OF_BIRTH, "%Y-%m-%d")
+//    FROM MEMBER_PROFILE
+//    WHERE MONTH(DATE_OF_BIRTH)=3
+//    AND GENDER='W'
+//    AND TLNO IS NOT NULL
+//    ORDER BY MEMBER_ID ASC;
 
 }
 
