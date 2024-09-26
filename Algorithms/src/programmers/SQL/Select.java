ea406c8d17d5666d326990b39c06285edc7aa828
김준섭 <wns1915@naver.com>
2024-09-26 17:20:21 +0900

    [feat] add 12세 이하인 여자 환자 목록 출력하기 [SQL]

--- Algorithms/src/programmers/SQL/Select.java
+++ Algorithms/src/programmers/SQL/Select.java
@@ -63,6 +63,12 @@ public class Select {
 //    FROM FOOD_FACTORY
 //    HAVING ADDRESS LIKE '강원도%'
 //    ORDER BY FACTORY_ID ASC;
+
+//    12세 이하인 여자 환자 목록 출력하기
+//    SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, "NONE")
+//    FROM PATIENT
+//    WHERE (AGE <= 12 AND GEND_CD='W')
+//    ORDER BY AGE DESC, PT_NAME ASC;
 }
 
 
