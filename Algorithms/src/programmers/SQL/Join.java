a4d3362ae48e3ec40734de36e78d154d1ca2e073
김준섭 <wns1915@naver.com>
2024-09-30 11:33:16 +0900

    [feat] add 그룹별 조건에 맞는 식당 목록 출력하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/Join.java
+++ Algorithms/src/programmers/SQL/Join.java
@@ -68,4 +68,23 @@ public class Join {
 //    ORDER BY
 //        A.PUBLISHED_DATE ASC;
 
+//    그룹별 조건에 맞는 식당 목록 출력하기
+//    SELECT
+//        A.MEMBER_NAME, B.REVIEW_TEXT, DATE_FORMAT(B.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+//    FROM
+//        MEMBER_PROFILE A
+//    JOIN
+//        REST_REVIEW B
+//        ON A.MEMBER_ID=B.MEMBER_ID
+//    WHERE
+//        B.MEMBER_ID = (
+//            SELECT MEMBER_ID
+//            FROM REST_REVIEW
+//            GROUP BY MEMBER_ID
+//            ORDER BY COUNT(*) DESC
+//            LIMIT 1
+//        )
+//    ORDER BY
+//        B.REVIEW_DATE ASC,
+//        B.REVIEW_TEXT ASC;
 }
