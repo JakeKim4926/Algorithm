5a6963af6e05666cde9d2fb0a74bfb11fc0099bb
김준섭 <wns1915@naver.com>
2024-10-01 21:26:28 +0900

    [feat] add 카테고리 별 도서 판매량 집계하기 [SQL]

---
+++ Algorithms/src/programmers/SQL/GroupBy.java
@@ -0,0 +1,18 @@
+package programmers.SQL;
+
+public class GroupBy {
+//    카테고리 별 도서 판매량 집계하기
+//    SELECT
+//        A.CATEGORY, SUM(B.SALES) AS TOTAL_SALES
+//    FROM
+//        BOOK A
+//    JOIN
+//        BOOK_SALES B
+//        ON A.BOOK_ID=B.BOOK_ID
+//    WHERE
+//        DATE_FORMAT(B.SALES_DATE, '%Y-%m')='2022-01'
+//    GROUP BY
+//        A.CATEGORY
+//    ORDER BY
+//        A.CATEGORY ASC;
+}
