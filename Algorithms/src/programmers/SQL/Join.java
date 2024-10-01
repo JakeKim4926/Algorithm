fe3ba6f92a4927cf42c7763389e157b6be5a72c3
김준섭 <wns1915@naver.com>
2024-10-01 19:53:53 +0900

    [feat] add 있었는데요 없었습니다. in Programmers [SQL]

--- Algorithms/src/programmers/SQL/Join.java
+++ Algorithms/src/programmers/SQL/Join.java
@@ -100,4 +100,17 @@ public class Join {
 //        A.ANIMAL_ID IS NULL
 //    ORDER BY
 //        B.ANIMAL_ID;
+
+//    있었는데요 없었습니다.
+//    SELECT
+//        A.ANIMAL_ID, A.NAME
+//    FROM
+//        ANIMAL_INS A
+//    JOIN
+//        ANIMAL_OUTS B
+//        ON A.ANIMAL_ID=B.ANIMAL_ID
+//    WHERE
+//        A.DATETIME >= B.DATETIME
+//    ORDER BY
+//        A.DATETIME ASC;
 }
