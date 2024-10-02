e69f911a370b0d71aeffea26df23218d335222ac
김준섭 <wns1915@naver.com>
2024-10-02 13:40:47 +0900

    [feat] add 오랜 기간 보호한 동물(1) in Programmers [SQL]

--- Algorithms/src/programmers/SQL/Join.java
+++ Algorithms/src/programmers/SQL/Join.java
@@ -113,4 +113,18 @@ public class Join {
 //        A.DATETIME >= B.DATETIME
 //    ORDER BY
 //        A.DATETIME ASC;
+
+//    오랜 기간 보호한 동물(1)
+//    SELECT
+//        A.NAME, A.DATETIME
+//    FROM
+//        ANIMAL_INS A
+//    LEFT JOIN
+//        ANIMAL_OUTS B
+//        ON A.ANIMAL_ID=B.ANIMAL_ID
+//    WHERE
+//        B.ANIMAL_ID IS NULL
+//    ORDER BY
+//        A.DATETIME
+//    LIMIT 3;
 }
