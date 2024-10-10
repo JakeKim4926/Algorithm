fa8a13710a7cda409d7c81268dbf392ad04d4cc1
김준섭 <wns1915@naver.com>
2024-10-10 10:23:32 +0900

    [feat] add 성분으로 구분한 아이스크림 총 주문량 in programmers [SQL]

--- Algorithms/src/programmers/SQL/GroupBy.java
+++ Algorithms/src/programmers/SQL/GroupBy.java
@@ -144,5 +144,19 @@ public class GroupBy {
 //    ORDER BY
 //        CAR_TYPE;
 //
+
+//    성분으로 구분한 아이스크림 총 주문량
+//    SELECT
+//        B.INGREDIENT_TYPE,
+//        SUM(A.TOTAL_ORDER) AS TOTAL_ORDER
+//    FROM
+//        FIRST_HALF A
+//    JOIN
+//        ICECREAM_INFO B
+//        ON A.FLAVOR=B.FLAVOR
+//    GROUP BY
+//        B.INGREDIENT_TYPE
+//    ORDER BY
+//        TOTAL_ORDER;
 }
 
