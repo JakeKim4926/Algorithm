481b966e1133c1fd7e16cee6cc153e3ab9282794
김준섭 <wns1915@naver.com>
2024-10-10 10:53:52 +0900

    [feat] add 조건에 맞는 사용자와 총 거래금액 조회하기 in programmers [SQL]

--- Algorithms/src/programmers/SQL/GroupBy.java
+++ Algorithms/src/programmers/SQL/GroupBy.java
@@ -172,5 +172,24 @@ public class GroupBy {
 //    ORDER BY
 //        5월예약건수,
 //        진료과코드;
+
+//    조건에 맞는 사용자와 총 거래금액 조회하기
+//    SELECT
+//        A.WRITER_ID,
+//        B.NICKNAME,
+//        SUM(A.PRICE) AS TOTAL_SALES
+//    FROM
+//        USED_GOODS_BOARD A
+//    JOIN
+//        USED_GOODS_USER B
+//        ON A.WRITER_ID=B.USER_ID
+//    WHERE
+//        A.STATUS='DONE'
+//    GROUP BY
+//        WRITER_ID
+//    HAVING
+//        SUM(PRICE) >= 700000
+//    ORDER BY
+//        TOTAL_SALES;
 }
 
