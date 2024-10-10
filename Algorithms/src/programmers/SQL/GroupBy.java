47dbfc88341d6b5c45a90d199d2517b28f2ed730
김준섭 <wns1915@naver.com>
2024-10-10 10:57:55 +0900

    [feat] add 고양이와 개는 몇 마리 있을까 in programmers [SQL]

--- Algorithms/src/programmers/SQL/GroupBy.java
+++ Algorithms/src/programmers/SQL/GroupBy.java
@@ -191,5 +191,16 @@ public class GroupBy {
 //        SUM(PRICE) >= 700000
 //    ORDER BY
 //        TOTAL_SALES;
+
+//    고양이와 개는 몇 마리 있을까
+//    SELECT
+//        ANIMAL_TYPE,
+//        COUNT(*) AS count
+//    FROM
+//        ANIMAL_INS
+//    GROUP BY
+//        ANIMAL_TYPE
+//    ORDER BY
+//        ANIMAL_TYPE;
 }
 
