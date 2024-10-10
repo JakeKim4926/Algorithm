b75cd2465447be5017ff980da9a2860cbca34815
김준섭 <wns1915@naver.com>
2024-10-10 10:27:31 +0900

    [feat] add 진료과별 총 예약 횟수 출력하기 in programmers [SQL]

--- Algorithms/src/programmers/SQL/GroupBy.java
+++ Algorithms/src/programmers/SQL/GroupBy.java
@@ -158,5 +158,19 @@ public class GroupBy {
 //        B.INGREDIENT_TYPE
 //    ORDER BY
 //        TOTAL_ORDER;
+
+//    진료과별 총 예약 횟수 출력하기
+//    SELECT
+//        MCDP_CD AS 진료과코드,
+//        COUNT(*) AS 5월예약건수
+//    FROM
+//        APPOINTMENT
+//    WHERE
+//        DATE_FORMAT(APNT_YMD, '%Y-%m')='2022-05'
+//    GROUP BY
+//        MCDP_CD
+//    ORDER BY
+//        5월예약건수,
+//        진료과코드;
 }
 
