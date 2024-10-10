832e7e96fba1b640ac2e0b0c336c1e35fdff21a0
김준섭 <wns1915@naver.com>
2024-10-10 09:10:25 +0900

    [feat] add 조건에 맞는 회원수 구하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/Select.java
+++ Algorithms/src/programmers/SQL/Select.java
@@ -119,6 +119,15 @@ public class Select {
 //        PRODUCT_ID,
 //        USER_ID;
 
+//    조건에 맞는 회원수 구하기
+//    SELECT
+//        COUNT(USER_ID) AS USERS
+//    FROM
+//            USER_INFO
+//    WHERE
+//        YEAR(JOINED)='2021'
+//        AND AGE BETWEEN 20 AND 29;
+
 }
 
 
