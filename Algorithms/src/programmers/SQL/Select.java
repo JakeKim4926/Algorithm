d8c9127537469762074fad7357036bee4d9cba3e
김준섭 <wns1915@naver.com>
2024-10-11 20:07:31 +0900

    [feat] add 업그레이드 된 아이템 구하기 in programmers [SQL]

--- Algorithms/src/programmers/SQL/Select.java
+++ Algorithms/src/programmers/SQL/Select.java
@@ -128,6 +128,27 @@ public class Select {
 //        YEAR(JOINED)='2021'
 //        AND AGE BETWEEN 20 AND 29;
 
+//    업그레이드 된 아이템 구하기
+//    SELECT
+//        ITEM_ID,
+//        ITEM_NAME,
+//        RARITY
+//    FROM
+//        ITEM_INFO
+//    WHERE
+//        ITEM_ID IN (
+//                SELECT
+//                    B.ITEM_ID
+//                FROM
+//                    ITEM_INFO A
+//                JOIN
+//                    ITEM_TREE B
+//                    ON A.ITEM_ID=B.PARENT_ITEM_ID
+//                WHERE
+//                    A.RARITY='RARE'
+//        )
+//    ORDER BY
+//        ITEM_ID DESC;
 }
 
 
