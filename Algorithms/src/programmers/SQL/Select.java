78d25e6127c61ba232da5270fe42d26a68a917e5
김준섭 <wns1915@naver.com>
2024-10-12 22:20:25 +0900

    [feat] add 조건에 맞는 개발자 찾기in programmers [SQL]

--- Algorithms/src/programmers/SQL/Select.java
+++ Algorithms/src/programmers/SQL/Select.java
@@ -149,6 +149,36 @@ public class Select {
 //        )
 //    ORDER BY
 //        ITEM_ID DESC;
+
+
+//    조건에 맞는 개발자 찾기
+//    SELECT
+//        ID,
+//        EMAIL,
+//        FIRST_NAME,
+//        LAST_NAME
+//    FROM
+//        DEVELOPERS
+//    WHERE
+//        SKILL_CODE & (
+//            SELECT
+//                CODE
+//            FROM
+//                SKILLCODES
+//            WHERE
+//                NAME='Python'
+//        )
+//        OR
+//        SKILL_CODE & (
+//            SELECT
+//                CODE
+//            FROM
+//                SKILLCODES
+//            WHERE
+//                NAME='C#'
+//        )
+//    ORDER BY
+//    ID;
 }
 
 
