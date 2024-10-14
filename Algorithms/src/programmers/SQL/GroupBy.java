86abc3c963198d7fd0113359015f4bcf6a9f23b1
김준섭 <wns1915@naver.com>
2024-10-14 18:36:29 +0900

    [feat] add 언어별 개발자 분류하기 in programmers [SQL]

--- Algorithms/src/programmers/SQL/GroupBy.java
+++ Algorithms/src/programmers/SQL/GroupBy.java
@@ -283,5 +283,27 @@ public class GroupBy {
 //    ORDER BY
 //        PRICE_GROUP;
 
+//    언어별 개발자 분류하기
+//    SELECT
+//        CASE
+//            WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME='PYTHON')
+//            AND SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY='Front End')
+//            THEN 'A'
+//            WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME='C#')
+//            THEN 'B'
+//            WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY='Front End')
+//            THEN 'C'
+//        END AS GRADE,
+//        ID,
+//        EMAIL
+//    FROM
+//        DEVELOPERS
+//    GROUP BY
+//        GRADE, ID, EMAIL
+//    HAVING
+//        GRADE IS NOT NULL
+//    ORDER BY
+//        GRADE,
+//        ID;
 }
 
