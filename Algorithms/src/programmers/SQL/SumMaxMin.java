cbd1f43d755e655d119ce5c0cc88739b90fb7666
김준섭 <wns1915@naver.com>
2024-10-15 10:21:01 +0900

    feat: add 잡은 물고기 중 가장 큰 물고기의 길이 구하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/SumMaxMin.java
+++ Algorithms/src/programmers/SQL/SumMaxMin.java
@@ -75,4 +75,12 @@ public class SumMaxMin {
 //            )
 //    ORDER BY
 //        A.ID;
+
+//    잡은 물고기 중 가장 큰 물고기의 길이 구하기
+//    SELECT
+//        CONCAT(MAX(LENGTH),'cm') AS MAX_LENGTH
+//    FROM
+//        FISH_INFO;
+
+    
 }
