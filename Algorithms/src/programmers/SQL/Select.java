a8f406b49394d63175c119eec9ffe4d86965d410
김준섭 <wns1915@naver.com>
2024-10-15 21:24:14 +0900

    feat: add 잔챙이 잡은 수 구하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/Select.java
+++ Algorithms/src/programmers/SQL/Select.java
@@ -194,6 +194,17 @@ public class Select {
 //        OR SKILL_3='Python'
 //    ORDER BY
 //        ID;
+
+
+//    잔챙이 잡은 수 구하기
+//    SELECT
+//        COUNT(*) AS FISH_COUNT
+//    FROM
+//        FISH_INFO
+//    WHERE
+//        LENGTH IS NULL;
+
+
 }
 
 
