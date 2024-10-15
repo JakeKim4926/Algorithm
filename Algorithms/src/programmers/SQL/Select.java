1bea3a319a516907360908542eb4163ca25fbeac
김준섭 <wns1915@naver.com>
2024-10-15 21:26:39 +0900

    feat: add 가장 큰 물고기 10마리 구하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/Select.java
+++ Algorithms/src/programmers/SQL/Select.java
@@ -204,7 +204,16 @@ public class Select {
 //    WHERE
 //        LENGTH IS NULL;
 
-
+//    가장 큰 물고기 10마리 구하기
+//    SELECT
+//        ID,
+//        LENGTH
+//    FROM
+//        FISH_INFO
+//    ORDER BY
+//        LENGTH DESC,
+//        ID
+//    LIMIT 10;
 }
 
 
