bb8a293f16271b2a6e8f6696690eede0f9c325eb
김준섭 <wns1915@naver.com>
2024-10-15 09:50:45 +0900

    feat: add 조건에 맞는 아이템들의 가격의 총합 구하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/SumMaxMin.java
+++ Algorithms/src/programmers/SQL/SumMaxMin.java
@@ -43,5 +43,11 @@ public class SumMaxMin {
 //    WHERE
 //        NAME IS NOT NULL;
 
-    
+//    조건에 맞는 아이템들의 가격의 총합 구하기
+//    SELECT
+//        SUM(PRICE) AS TOTAL_PRICE
+//    FROM
+//        ITEM_INFO
+//    WHERE
+//        RARITY='LEGEND';
 }
