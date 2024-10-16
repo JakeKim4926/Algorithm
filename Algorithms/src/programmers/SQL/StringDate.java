c087c658be101b41920b956c6777bdea77a53a81
김준섭 <wns1915@naver.com>
2024-10-16 11:21:25 +0900

    feat: add 자동차 대여 기록에서 장기/단기 대여 구분하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/StringDate.java
+++ Algorithms/src/programmers/SQL/StringDate.java
@@ -96,7 +96,7 @@ public class StringDate {
 //        BOARD_ID DESC;
 
 //    특정 옵션이 포함된 자동차 리스트 구하기
-//    SELECT 
+//    SELECT
 //        *
 //    FROM
 //        CAR_RENTAL_COMPANY_CAR
@@ -104,4 +104,22 @@ public class StringDate {
 //        OPTIONS LIKE '%네비게이션%'
 //    ORDER BY
 //        CAR_ID DESC;
+
+//    자동차 대여 기록에서 장기/단기 대여 구분하기
+//    SELECT
+//        HISTORY_ID,
+//        CAR_ID,
+//        DATE_FORMAT(START_DATE, '%Y-%m-%d'),
+//        DATE_FORMAT(END_DATE, '%Y-%m-%d'),
+//        CASE
+//            WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= 30 THEN '장기 대여'
+//            ELSE '단기 대여'
+//        END AS RENT_TYPE
+//    FROM
+//        CAR_RENTAL_COMPANY_RENTAL_HISTORY
+//    WHERE
+//        DATE_FORMAT(START_DATE, '%Y-%m')='2022-09'
+//    ORDER BY
+//        HISTORY_ID DESC;
+    
 }
