3b886e60d598c26c5a6444e7bd9f5528114ea00e
김준섭 <wns1915@naver.com>
2024-10-16 11:26:23 +0900

    feat: add 조건별로 분류하여 주문상태 출력하기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/StringDate.java
+++ Algorithms/src/programmers/SQL/StringDate.java
@@ -121,5 +121,21 @@ public class StringDate {
 //        DATE_FORMAT(START_DATE, '%Y-%m')='2022-09'
 //    ORDER BY
 //        HISTORY_ID DESC;
-    
+
+
+//    조건별로 분류하여 주문상태 출력하기
+//    SELECT
+//        ORDER_ID,
+//        PRODUCT_ID,
+//        DATE_FORMAT(OUT_DATE, '%Y-%m-%d'),
+//        CASE
+//            WHEN OUT_DATE < '2022-05-02' THEN '출고완료'
+//            WHEN OUT_DATE IS NULL THEN '출고미정'
+//            ELSE '출고대기'
+//        END AS 출고여부
+//    FROM
+//        FOOD_ORDER
+//    ORDER BY
+//        ORDER_ID;
+
 }
