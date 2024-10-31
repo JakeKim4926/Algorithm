cf9cde83ec71db1094f16e21321649521b4132aa
김준섭 <wns1915@naver.com>
2024-10-31 09:46:28 +0900

    feat: add 루시와 엘라 찾기 in Programmers [SQL]

--- Algorithms/src/programmers/SQL/StringDate.java
+++ Algorithms/src/programmers/SQL/StringDate.java
@@ -175,4 +175,16 @@ public class StringDate {
 //    ORDER BY
 //        USER_ID DESC;
 
+//    루시와 엘라 찾기
+//    SELECT
+//        ANIMAL_ID,
+//        NAME,
+//        SEX_UPON_INTAKE
+//    FROM
+//        ANIMAL_INS
+//    WHERE
+//        NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+//    ORDER BY
+//        ANIMAL_ID;
+
 }
